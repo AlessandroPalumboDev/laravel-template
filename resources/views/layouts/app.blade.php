a794205e541e22a36918602099ede83573717bfa
Alessandro Palumbo <163033493+AlessandroPalumboDev@users.noreply.github.com>
2024-07-09 12:07:11 +0200

    [FIX] Cambio titolo app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel Boolando</title>
+    <title>Laravel Template</title>
     <link rel="icon" href="{{ Vite::asset('resources/img/pipistrello.jpg') }}">
 
     @vite('resources/js/app.js')
