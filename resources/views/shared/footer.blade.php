ac33071dbc9743256dd242e532bf103b9da81588
Alessandro Palumbo <163033493+AlessandroPalumboDev@users.noreply.github.com>
2024-07-09 11:58:48 +0200

    [UPD] Creato layout base con header e footer condiviso

---
+++ resources/views/shared/footer.blade.php
@@ -0,0 +1,8 @@
+<footer>
+
+    <span class="rock-salt-regular text-secondary p-1 m-1">
+        <i class="fas fa-yin-yang"></i> AP.
+    </span>
+
+
+</footer>
---
+++ resources/views/shared/header.blade.php
@@ -0,0 +1,11 @@
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>
+                    Header Template
+                </h1>
+            </div>
+        </div>
+    </div>
+</header>
