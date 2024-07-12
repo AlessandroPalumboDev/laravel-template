3edb6e724a994c1945b41b3917360ebd80787b7f
Alessandro Palumbo <163033493+AlessandroPalumboDev@users.noreply.github.com>
2024-07-12 14:53:26 +0200

    [UPD] Aggiornamento header

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -1,11 +1,17 @@
-<header>
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <h1>
-                    Header Template
-                </h1>
-            </div>
+<header class="py-2">
+    <nav class="navbar navbar-expand-lg bg-black">
+        <div class="container-fluid">
+          <a class="navbar-brand text-light" href="{{route('home')}}">Home</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="text-light" aria-current="page" href="#">Pagina</a>
+              </li>
+          </div>
         </div>
-    </div>
-</header>
+      </nav>
+  </header>
+  
